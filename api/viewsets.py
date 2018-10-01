7711f95f7b66cc49c24d8886de038e212c18022f
clara <clara@minneapolis.edu>
2018-10-01 12:34:00 -0500

    filter for active students in grader launch

--- api/viewsets.py
+++ api/viewsets.py
@@ -12,7 +12,7 @@ class AssignmentViewSet(viewsets.ModelViewSet):
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all().order_by('-programming_class__semester_code', Lower('name'))
     serializer_class = StudentSerializer
-    filter_fields = ('name', 'programming_class')
+    filter_fields = ('name', 'programming_class', 'active')
 
 class ProgrammingClassViewSet(viewsets.ModelViewSet):
     queryset = ProgrammingClass.objects.all().order_by('-semester_code')
