cb3cae77502b34a839e693bbc5e22d43c33a31d0
clara <clara@minneapolis.edu>
2018-10-25 15:13:05 -0500

    editing instructor comments, adjusted points

--- grader_client/src/components/grades/GradeResultDetail.vue
+++ grader_client/src/components/grades/GradeResultDetail.vue
@@ -26,7 +26,7 @@ individual questions, and for the whole assignment  -->
 
   <p><span class="title">Date graded:</span> {{ result.date | moment('ddd MMM YYYY hh:mm a')}}</p>
   <p><span class="title">Student GitHub:</span> <a v-bind:href="result.student_github_url">{{ result.student_github_url}}</a></p>
-  <p><span class="title">Grade:</span> {{ report.total_points_earned }} out of {{ report.total_points_available }}</p>
+  <p><span class="title">Calculated grade:</span> {{ report.total_points_earned | toFixed(2) }} out of {{ report.total_points_available }}</p>
     <!-- <P>Generated Report {{result.generated_report}}</p> -->
     <!-- <GeneratedGradeReport v-bind:report="result.generated_report"></GeneratedGradeReport> -->
 
@@ -37,9 +37,9 @@ individual questions, and for the whole assignment  -->
   -->
 
   <p v-for="qr in report.question_reports">
-    <ul>
+    <ul id="why">
         <p class="question-header"><span class="title">Question:</span> {{qr.question.question}}</p>
-        <p><span class="title">Points avail:</span>  {{qr.points_available}}</p>
+        <p><span class="title">Points available:</span>  {{qr.points_available}}</p>
         <p><span class="title">Source file:</span>  {{qr.source_file}}</p>
         <p><span class="title">Test files:</span>
           <ul><li v-for="f in qr.question.test_files"> {{f}} </li></ul>
@@ -53,10 +53,10 @@ individual questions, and for the whole assignment  -->
 
         <p><span class="title">Test Suites:</span></p>
           <ul><li v-for="tss in qr.testsuites">
-             Name: {{tss.name}}, Tests: {{tss.tests}}, Fails: {{tss.failures}}, Errors: {{tss.errors}}, Passes: {{tss.passes}}
+             <span class="testsuite-name">{{tss.name}}:</span> {{tss.tests}} Tests, {{tss.failures}} Fails, {{tss.errors}} Errors, {{tss.passes}} Passes.
 
             <ul><li v-for="ts in tss.testsuites">
-               Name: {{ts.name}}, Tests: {{ts.tests}}, Fails: {{ts.failures}}, Errors: {{ts.errors}}, Passes: {{ts.passes}}
+               <span class="testsuite-name">{{ts.name}}:</span> {{ts.tests}} Tests, {{ts.failures}} Fails, {{ts.errors}} Errors, {{ts.passes}} Passes.
 
               <ul><li v-for="tc in ts.testcases">
                 <span class="testcase-name">{{tc.name}}</span> <span v-if="tc.passed" class="passed">Passed</span>
@@ -66,23 +66,38 @@ individual questions, and for the whole assignment  -->
             </li></ul>
           </li></ul>
 
-        <p><span class="title">Points earned for question:</span> {{qr.points_earned}}</p>
+        <p><span class="title">Points earned for question:</span> {{qr.points_earned | toFixed(2) }}</p>
 
-        <p><span class="title">Instructor comments for question: </span> <textarea v-model="qr.instructor_comments"></textarea></p>
-        <p><span class="title">Adjusted points: </span> <input v-model="qr.adjusted_points" /></p>
+        <p><span class="title">Instructor comments for question: </span> <textarea class="question-comments" v-model="qr.instructor_comments" v-on:change="updateQuestionComments()"></textarea></p>
+        <p><span class="title">Adjusted points: </span> <input v-on:change="updateAdjustedPoints(qr.adjusted_points)" v-model="qr.adjusted_points" />
+          <span class="warning" v-if="qr.adjusted_points < 0 || qr.adjusted_points > report.total_points_available">
+            Adjusted points not valid
+          </span>
+        </p>
       </ul>
     </p>
 
+    <div>
+    <p class="section-title">Summary</p>
+
+    <p><span class="title">Calculated grade:</span> {{ report.total_points_earned | toFixed(2) }} out of {{ report.total_points_available }}</p>
+    <p><span class="title">Total adjusted points: </span>{{totalAdjustedPoints | toFixed(2) }} out of {{ report.total_points_available }}
+      <span class="warning" v-if="totalAdjustedPoints < 0 || totalAdjustedPoints > report.total_points_available">
+        Adjusted points not valid
+      </span>
+    </p>
+
     <P><span class="title">Overall Instructor Comments:</span>
-      <textarea id="overall-comments" v-model="report.overall_instructor_comments" v-on:change="updateInstructorComments">
+      <textarea id="overall-comments" v-model="report.overall_instructor_comments" v-on:change="updateOverallInstructorComments">
       </textarea></P>
 
       <p>All looks good?</p> <input type="checkbox" v-model="lgtm" v-on:click="allGood()"/>
 
     </div>
+    </div>
 
     <div id="buttons">
-    <button id="save">SAVE</button>
+    <button id="save" v-on:click="save()">SAVE</button>
     <br>
     <button id="d2l-report">D2L Report</button>
     </div>
@@ -103,12 +118,21 @@ export default {
       result: {},
       report: {},
       lgtm: false,
-      backupOverall: ''
+      backupOverall: '',
+      totalAdjustedPoints: 400
+    }
+  },
+  filters: {
+    toFixed: function(value, decimal_places) {
+      if (value && !isNaN(value)) {
+        return value.toFixed(decimal_places)
+      }
+      return value
     }
   },
   mounted() {
 
-    console.log('my object is', this.result)
+    console.log('my result object is', this.result)
 
     let id = this.$route.query.id
     this.$grade_backend.$fetchOne(id).then(data => {
@@ -126,22 +150,72 @@ export default {
       this.report = JSON.parse(this.result.generated_report)
       console.log('The grade report is ', this.report)
 
+      this.updateTotalAdjustedPoints()
+
     }).catch( err => {
       console.log('errrrr', err)
     })
 },
 
 methods: {
-  updateInstructorComments() {
-    // Stringify the whole report and send it back to server
-    let stringReport = JSON.Stringify(this.report)
-    this.result.report = stringReport
-    this.$grade_backend.$editItem(result.id, this.result).then( () => {
+
+  save() {
+
+    console.log('save', this.result.id)
+    let stringReport = JSON.stringify(this.report)
+    this.result.generated_report = stringReport
+    this.$grade_backend.$editItem(this.result).then( () => {
       console.log('saved, hopefully. ')
-    }
-  )
+    })
+  },
+  updateQuestionComments(comment) {
+    this.save();
+  },
+  updateOverallInstructorComments(comment) {
+    // Stringify the whole report and send it back to server
+    // TODO waqqAQvalidationq`1 here if needed
+    this.save()
+  },
+
+  updateAdjustedPoints(pts) {
+    // TODO validation, is number, no negative points
+      if (isNaN(pts)) {
+        alert('numbers only!')
+        return
+      }
+
+      if (pts < 0) {
+        alert('positive numbers please')
+      }
+
+      //update total adjusted
+
+      this.updateTotalAdjustedPoints()
+
+      this.save()
+  },
+
+  updateTotalAdjustedPoints() {
+
+    console.log('adjusting total for ', this.report)
+
+    this.report.question_reports.forEach( q => {
+      if (!q.adjusted_points) {
+        q.adjusted_points = q.points_earned.toFixed(2)
+      }
+    })
+
+
+    let sum = this.report.question_reports.reduce( (s, q) => {
+      console.log(s, q.adjusted_points)
+      return Number(s.adjusted_points) + Number(q.adjusted_points)
+    })
+
+    this.totalAdjustedPoints = sum
+
 
   },
+
   allGood() {
     console.log(this.lgtm)
 
@@ -161,6 +235,10 @@ methods: {
 </script>
 
 <style>
+
+#why {
+  padding-left: 0px;
+}
 
 #content {
   text-align: left;
@@ -197,12 +275,26 @@ p {
   font-weight: bold;
 }
 
+.section-title {
+  font-size: 20px;
+  font-weight: bold;
+}
+
 #overall-comments {
   width: 100%;
 }
 
+.question-comments {
+    width: 100%;
+}
+
+
 .testcase-name {
   font-style: italic;
+  font-weight: bold;
+}
+
+.testsuite-name {
   font-weight: bold;
 }
 
@@ -215,7 +307,7 @@ p {
   position: fixed;
   top: 0;
   right: 0;
-  margin: 100px;
+  margin: 30px;
   /* width: 100; */
   height: 50px;
   background-color: orange;
@@ -225,7 +317,14 @@ p {
 
 button {
   width: 100px;
-
 }
+
+.warning {
+  font-style: italic;
+  font-weight: bold;
+  color: darkred;
+}
+
+
 
 </style>
